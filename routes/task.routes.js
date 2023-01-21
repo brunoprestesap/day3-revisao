3b1b80f98999ec0ab3c7f429411da974f28ffc29
Bruno Prestes <brunoasprestes@gmail.com>
2023-01-21 15:02:18 -0300

    criacao do crud completa

--- routes/task.routes.js
+++ routes/task.routes.js
@@ -1,107 +1,103 @@
 import express from "express";
-import TaskModel from '../model/task.model.js'
-import UserModel from '../model/user.model.js'
+import TaskModel from "../model/task.model.js";
+import UserModel from "../model/user.model.js";
 
-const taskRoute = express.Router()
+const taskRoute = express.Router();
 
 taskRoute.post("/create-task/:idUser", async (req, res) => {
-    try {
-        const { idUser } = req.params
+  try {
+    const { idUser } = req.params;
 
-        const newTask = await TaskModel.create({ ...req.body, user: idUser })
+    const newTask = await TaskModel.create({ ...req.body, user: idUser });
 
-        const userUpdated = await UserModel.findByIdAndUpdate(
-            idUser,
-            {
-                $push: {
-                    tasks: newTask._id
-                }
-            },
-            { new: true, runValidators: true}
-        )
-        return res.status(201).json(newTask)
-    } catch (error) {
-        console.log(error)
-        return res.status(400).json(error.errors)
-    }
-})
+    const userUpdated = await UserModel.findByIdAndUpdate(
+      idUser,
+      {
+        $push: {
+          tasks: newTask._id,
+        },
+      },
+      { new: true, runValidators: true }
+    );
+    return res.status(201).json(newTask);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error.errors);
+  }
+});
 
 taskRoute.get("/onetask/:id", async (req, res) => {
-    try {
+  try {
+    const { id } = req.params;
 
-        const { id } = req.params
+    const task = await TaskModel.findById(id).populate("user");
 
-        const task = await TaskModel.findById(id).populate('user')
-
-        if (!task) {
-            return res.status(400).json({ msg: "Tarefa não encontrado!" })
-        }
-
-        return res.status(200).json(task)
-
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json(error.errors)
+    if (!task) {
+      return res.status(400).json({ msg: "Tarefa não encontrado!" });
     }
-})
 
-taskRoute.get('/all-tasks', async (req, res) => {
-    try {
+    return res.status(200).json(task);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error.errors);
+  }
+});
 
-        const tasks = await TaskModel.find({}, { __v: 0, updatedAt: 0 })
-        return res.status(200).json(tasks)
+taskRoute.get("/all-tasks", async (req, res) => {
+  try {
+    const tasks = await TaskModel.find({}, { __v: 0, updatedAt: 0 });
+    return res.status(200).json(tasks);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error.errors);
+  }
+});
 
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json(error.errors)
+taskRoute.put("/edit/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const updatedTask = await TaskModel.findOneAndUpdate(
+      { _id: id },
+      { ...req.body },
+      { new: true, runValidators: true }
+    );
+
+    return res.status(200).json(updatedTask);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error.errors);
+  }
+});
+
+taskRoute.delete("/delete/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deletedTask = await TaskModel.findByIdAndDelete(id);
+
+    if (!deletedTask) {
+      return res.status(404).json({ msg: "Task não encontrada!" });
     }
-})
 
-taskRoute.put('/edit/:id', async (req, res) => {
-    try {
-        const { id } = req.params
+    const userUpdated = await UserModel.findByIdAndUpdate(
+      deletedTask.user,
+      {
+        $pull: {
+          tasks: deletedTask._id,
+        },
+      },
+      {
+        new: true,
+        runValidators: true,
+      }
+    );
 
-        const updatedTask = await TaskModel.findOneAndUpdate(
-            { _id: id},
-            { ...req.body },
-            { new: true, runValidators: true }
-        );
+    return res.status(200).json(deletedTask);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error.errors);
+  }
+});
 
-        return res.status(200).json(updatedTask)
-
-    } catch (error) {
-        console.log(error)
-        return res.status(400).json(error.errors)
-    }
-})
-
-taskRoute.delete('/delete/:id', async (req, res) => {
-    try {
-
-        const { id } = req.params
-
-        const deletedTask = await TaskModel.findByIdAndDelete(id)
-
-        if (!deletedTask) {
-            return res.status(404).json({ msg: "Task não encontrada!" })
-        }
-
-        const userUpdated = await UserModel.findByIdAndUpdate(deletedTask.user,
-            {
-                $pull: {
-                    tasks: deletedTask._id
-                }
-            }, {
-                new: true, runValidators: true
-            })
-
-        return res.status(200).json(deletedTask)
-
-    } catch (error) {
-        console.log(error)
-        return res.status(400).json(error.errors)
-    }
-})
-
-
-export default taskRoute
+export default taskRoute;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,91 +1,86 @@
-import express from 'express'
-import TaskModel from '../model/task.model.js'
-import UserModel from '../model/user.model.js'
+import express from "express";
+import TaskModel from "../model/task.model.js";
+import UserModel from "../model/user.model.js";
 
-const userRoute = express.Router()
+const userRoute = express.Router();
 
 //CREATE - CRUD
-userRoute.post('/create-user', async (req, res) => {
-    try {
+userRoute.post("/create-user", async (req, res) => {
+  try {
+    const form = req.body;
+    const newUser = await UserModel.create(form);
+    return res.status(201).json(newUser);
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ msg: "Algo deu errado na criação do usuário" });
+  }
+});
 
-        const form = req.body
-        const newUser = await UserModel.create(form)
-        return res.status(201).json(newUser)
+userRoute.get("/all-users", async (req, res) => {
+  try {
+    const users = await UserModel.find({}, { __v: 0, updatedAt: 0 }).sort({
+      age: 1,
+    });
+    return res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error.errors);
+  }
+});
 
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json({ msg: "Algo deu errado na criação do usuário" })
+userRoute.get("/one-user/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const user = await UserModel.findById(id).populate("tasks");
+
+    if (!user) {
+      return res.status(400).json({ msg: "Usuário não encontrado!" });
     }
-})
 
-userRoute.get('/all-users', async (req, res) => {
-    try {
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error.errors);
+  }
+});
 
-        const users = await UserModel.find({}, { __v: 0, updatedAt: 0 }).sort({ age: 1 })
-        return res.status(200).json(users)
+userRoute.put("/update/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
 
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json(error.errors)
+    const updatedUser = await UserModel.findByIdAndUpdate(
+      id,
+      { ...req.body },
+      { new: true, runValidators: true }
+    );
+
+    return res.status(200).json(updatedUser);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error.errors);
+  }
+});
+
+userRoute.delete("/delete/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const deletedUser = await UserModel.findByIdAndDelete(id);
+
+    if (!deletedUser) {
+      return res.status(400).json({ msg: "Usuário não encontrado!" });
     }
-})
 
-userRoute.get("/oneuser/:id", async (req, res) => {
-    try {
+    await TaskModel.deleteMany({ user: id });
 
-        const { id } = req.params
+    return res.status(200).json(deletedUser);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json(error.errors);
+  }
+});
 
-        const user = await UserModel.findById(id).populate("tasks")
-
-        if (!user) {
-            return res.status(400).json({ msg: "Usuário não encontrado!" })
-        }
-
-        return res.status(200).json(user)
-
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json(error.errors)
-    }
-})
-
-userRoute.put('/update/:id', async (req, res) => {
-    try {
-        const { id } = req.params
-
-        const updatedUser = await UserModel.findByIdAndUpdate(
-            id,
-            { ...req.body },
-            { new: true, runValidators: true }
-        );
-
-        return res.status(200).json(updatedUser)
-
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json(error.errors)
-    }
-})
-
-userRoute.delete('/delete/:id', async (req, res) => {
-    try {
-
-        const { id } = req.params
-
-        const deletedUser = await UserModel.findByIdAndDelete(id)
-
-        if (!deletedUser) {
-            return res.status(404).json({ msg: "Usuário não encontrado!" })
-        }
-
-        await TaskModel.deleteMany({ user: id })
-
-        return res.status(200).json(deletedUser)
-
-    } catch (error) {
-        console.log(error)
-        return res.status(500).json(error.errors)
-    }
-})
-
-export default userRoute
+export default userRoute;
